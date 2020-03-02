6b2c1aa80f684b46d14d0556685fd7ac51f87c14
xiaokelinxiaoke <linxiaokexiaoke@sina.com>
2020-03-02 21:02:49 +0800

    Update moc_mainwindow.cpp

--- src/HWaterMark/HWaterMark/moc_mainwindow.cpp
+++ src/HWaterMark/HWaterMark/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[15];
-    char stringdata0[301];
+    QByteArrayData data[17];
+    char stringdata0[322];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -45,7 +45,9 @@ QT_MOC_LITERAL(10, 184, 31), // "on_comboBox_currentIndexChanged"
 QT_MOC_LITERAL(11, 216, 21), // "on_comboBox_activated"
 QT_MOC_LITERAL(12, 238, 23), // "on_comboBox_2_activated"
 QT_MOC_LITERAL(13, 262, 32), // "on_horizontalSlider_valueChanged"
-QT_MOC_LITERAL(14, 295, 5) // "value"
+QT_MOC_LITERAL(14, 295, 5), // "value"
+QT_MOC_LITERAL(15, 301, 11), // "addmarkSlot"
+QT_MOC_LITERAL(16, 313, 8) // "idftSlot"
 
     },
     "MainWindow\0on_pushButton_clicked\0\0"
@@ -56,7 +58,8 @@ QT_MOC_LITERAL(14, 295, 5) // "value"
     "on_lineEdit_cursorPositionChanged\0"
     "arg1\0arg2\0on_comboBox_currentIndexChanged\0"
     "on_comboBox_activated\0on_comboBox_2_activated\0"
-    "on_horizontalSlider_valueChanged\0value"
+    "on_horizontalSlider_valueChanged\0value\0"
+    "addmarkSlot\0idftSlot"
 };
 #undef QT_MOC_LITERAL
 
@@ -66,7 +69,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      10,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -74,16 +77,18 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   64,    2, 0x08 /* Private */,
-       3,    0,   65,    2, 0x08 /* Private */,
-       4,    0,   66,    2, 0x08 /* Private */,
-       5,    0,   67,    2, 0x08 /* Private */,
-       6,    0,   68,    2, 0x08 /* Private */,
-       7,    2,   69,    2, 0x08 /* Private */,
-      10,    1,   74,    2, 0x08 /* Private */,
-      11,    1,   77,    2, 0x08 /* Private */,
-      12,    1,   80,    2, 0x08 /* Private */,
-      13,    1,   83,    2, 0x08 /* Private */,
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    0,   75,    2, 0x08 /* Private */,
+       4,    0,   76,    2, 0x08 /* Private */,
+       5,    0,   77,    2, 0x08 /* Private */,
+       6,    0,   78,    2, 0x08 /* Private */,
+       7,    2,   79,    2, 0x08 /* Private */,
+      10,    1,   84,    2, 0x08 /* Private */,
+      11,    1,   87,    2, 0x08 /* Private */,
+      12,    1,   90,    2, 0x08 /* Private */,
+      13,    1,   93,    2, 0x08 /* Private */,
+      15,    0,   96,    2, 0x08 /* Private */,
+      16,    0,   97,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -96,6 +101,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::QString,    8,
     QMetaType::Void, QMetaType::QString,    8,
     QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -116,6 +123,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 7: _t->on_comboBox_activated((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         case 8: _t->on_comboBox_2_activated((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         case 9: _t->on_horizontalSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->addmarkSlot(); break;
+        case 11: _t->idftSlot(); break;
         default: ;
         }
     }
@@ -146,13 +155,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 10)
+        if (_id < 12)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 10;
+        _id -= 12;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 10)
+        if (_id < 12)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 10;
+        _id -= 12;
     }
     return _id;
 }
